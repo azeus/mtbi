08f241ae1fc2a28dc3fae1d261f8cc0795c55853
azeus <azmath.gr@gmail.com>
2025-05-12 16:28:46 -0700

    utils

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ st.set_page_config(
 import random
 import time
 import os
-from utils import MBTI_TYPES, MBTI_AVATARS, get_type_nickname, get_type_description, get_type_cognitive_functions, \
-    simulate_mbti_response
+from utils import MBTI_TYPES, MBTI_AVATARS, get_type_nickname, get_type_description, get_type_cognitive_functions, simulate_mbti_response
 
 
 # Optional imports - will be used if files exist
